c560bde0694a3d1176e6dd768570e6999da50e53
dinar <dinar.shayhyd@gmail.com>
2023-11-22 13:19:39 +0300

    Сделал граф, поис в ширину и глубину

---
+++ bfs.py
@@ -0,0 +1,73 @@
+from typing import Callable
+
+from Set import MySet
+from Queue import MyQueue
+from graph import Graph, T, AdjacentEdge, Edge
+
+
+def bfs(graph: Graph[T], start: T, walkfunc: Callable[[T], bool]) -> None:
+    queue = MyQueue[T]()
+    visited = MySet[T]()
+    queue.enqueue(start)
+
+    def __foreach(edge: AdjacentEdge[T]) -> None:
+        if not visited.contains(edge.finish_edge):
+            queue.enqueue(edge.finish_edge)
+
+    while not queue.is_empty():
+        vertex = queue.dequeue()
+
+        if walkfunc(vertex):
+            return
+
+        visited.add(vertex)
+
+        graph.for_each_adjacent_edge(vertex, __foreach)
+
+
+if __name__ == '__main__':
+    vertexes: list[Edge[str]] = [
+        Edge("A", "B"),
+        Edge("A", "C"),
+        Edge("C", "F"),
+        Edge("C", "G"),
+        Edge("G", "M"),
+        Edge("G", "N"),
+        Edge("B", "D"),
+        Edge("B", "E"),
+        Edge("D", "H"),
+        Edge("D", "I"),
+        Edge("D", "J"),
+        Edge("E", "K"),
+        Edge("E", "L"),
+    ]
+
+    graph: Graph[str] = Graph[str]()
+
+    for it in vertexes:
+        graph.add_edge_without_weight(it.start_edge, it.finish_edge)
+
+    def bfs_walk(vertex: str) -> bool:
+        print(vertex + " ", end='')
+        return vertex == "K"
+
+    bfs(graph, "A", bfs_walk)
+    print()
+    path: list[str] = []
+
+    def bfs_walk_with_path(vertex: str) -> bool:
+        path.append(vertex)
+        return vertex == "K"
+
+    bfs(graph, "A", bfs_walk_with_path)
+
+    path.reverse()
+    min_path: list[str] = ["K"]
+    find: str = "K"
+    for vertex in path:
+        if graph.vertexes[vertex].contains(AdjacentEdge[str](find)):
+            min_path.append(vertex)
+            find = vertex
+
+    min_path.reverse()
+    print(min_path)
---
+++ dfs.py
@@ -0,0 +1,78 @@
+from typing import Callable
+
+from Set import MySet
+from graph import Graph, T, AdjacentEdge, Edge
+
+__is_found: bool = False
+
+
+def dfs(graph: Graph[T], start: T, walkfunc: Callable[[T], bool]) -> None:
+    global __is_found
+    visited = MySet[T]()
+    __is_found = False
+    __dfs(graph, start, visited, walkfunc)
+
+
+def __dfs(graph: Graph[T], start: T, visited: MySet[T], walkfunc: Callable[[T], bool]) -> None:
+    global __is_found
+    visited.add(start)
+
+    __is_found = walkfunc(start)
+    if __is_found:
+        return
+
+    def __foreach(edge: AdjacentEdge[T]) -> None:
+        if __is_found:
+            return
+        if not visited.contains(edge.finish_edge):
+            __dfs(graph, edge.finish_edge, visited, walkfunc)
+
+    graph.for_each_adjacent_edge(start, __foreach)
+
+
+if __name__ == '__main__':
+    vertexes: list[Edge[str]] = [
+        Edge("A", "B"),
+        Edge("A", "C"),
+        Edge("C", "F"),
+        Edge("C", "G"),
+        Edge("G", "M"),
+        Edge("G", "N"),
+        Edge("B", "D"),
+        Edge("B", "E"),
+        Edge("D", "H"),
+        Edge("D", "I"),
+        Edge("D", "J"),
+        Edge("E", "K"),
+        Edge("E", "L"),
+    ]
+
+    graph: Graph[str] = Graph[str]()
+
+    for it in vertexes:
+        graph.add_edge_without_weight(it.start_edge, it.finish_edge)
+
+    def dfs_walk(vertex: str) -> bool:
+        print(vertex + " ", end='')
+        return vertex == "K"
+
+    dfs(graph, "A", dfs_walk)
+    path: list[str] = []
+
+    def dfs_walk_with_path(vertex: str) -> bool:
+        path.append(vertex)
+        return vertex == "K"
+
+    dfs(graph, "A", dfs_walk_with_path)
+    print()
+
+    path.reverse()
+    min_path: list[str] = ["K"]
+    find: str = "K"
+    for vertex in path:
+        if graph.vertexes[vertex].contains(AdjacentEdge[str](find)):
+            min_path.append(vertex)
+            find = vertex
+
+    min_path.reverse()
+    print(min_path)
